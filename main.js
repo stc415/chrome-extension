ef2bbe164308495b0d1f6e9760966546d8f17792
Neul Seo <neul.seo.1213@gmail.com>
2024-02-14 21:26:12 -0500

    made the clock

---
+++ main.js
@@ -0,0 +1,14 @@
+// grab the html elements
+// const hour = document.querySelector("#hour");
+
+// how do we want to display the counting down time?
+// get the current time
+// subtract the countdown time --> question: is it gonna be in min? sec? milisec?
+// display the countdown on the countdown box
+const currTime = new Date();
+console.log(currTime);
+// convert hours and minutes into seconds
+
+// convert the user input to seconds
+// conditional: check if current time = current time + input time
+// if so, execute the code
