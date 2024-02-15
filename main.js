923574122674220a804e8bc68a05f0190f1b5a87
Neul Seo <neul.seo.1213@gmail.com>
2024-02-15 07:51:15 -0500

    commit

--- main.js
+++ main.js
@@ -1,13 +1,10 @@
-// grab the html elements
-// const hour = document.querySelector("#hour");
-
 // how do we want to display the counting down time?
-// get the current time
-// subtract the countdown time --> question: is it gonna be in min? sec? milisec?
+// get the current time, subtract the countdown time --> question: is it gonna be in min? sec? milisec?
 // display the countdown on the countdown box
-const currTime = new Date();
+const currTime = new Date(); // current time in milliseconds
 console.log(currTime);
 // convert hours and minutes into seconds
+const hour = currTime;
 
 // convert the user input to seconds
 // conditional: check if current time = current time + input time
