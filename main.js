d6d50c092631e688049e203a059c3e24e8d63c4d
Neul Seo <neul.seo.1213@gmail.com>
2024-02-15 07:56:47 -0500

    merged

--- main.js
+++ main.js
@@ -1,10 +1,11 @@
+// grab the html elements
+// const hour = document.querySelector("#hour");
+
 // how do we want to display the counting down time?
-// get the current time, subtract the countdown time --> question: is it gonna be in min? sec? milisec?
+// get the current time
+// subtract the countdown time --> question: is it gonna be in min? sec? milisec?
 // display the countdown on the countdown box
-const currTime = new Date(); // current time in milliseconds
-console.log(currTime);
 // convert hours and minutes into seconds
-const hour = currTime;
 
 // convert the user input to seconds
 // conditional: check if current time = current time + input time
@@ -17,8 +18,8 @@ const startButton = document.querySelector("#btn");
 
 startButton.addEventListener("click", function () {
   const seconds = parseInt(secondInput.value) || 0;
-  const minutes = parseInt(minuteInput.value || 0) * 60;
-  const hours = parseInt(hourInput.value || 0) * 60 * 60;
+  const minutes = parseInt((minuteInput.value) || 0) * 60;
+  const hours = parseInt((hourInput.value) || 0) * 60 * 60;
   const totalSeconds = seconds + minutes + hours;
 
   startCountdown(totalSeconds);
@@ -35,21 +36,15 @@ function startCountdown(time) {
     // console.log('Seconds: ', seconds);
     // console.log('minutes: ', minutes);
 
-    document.querySelector(".countdown-hour").textContent = String(
-      hours
-    ).padStart(2, "0");
-    document.querySelector(".countdown-minute").textContent = String(
-      minutes
-    ).padStart(2, "0");
-    document.querySelector(".countdown-second").textContent = String(
-      seconds
-    ).padStart(2, "0");
+    document.querySelector(".countdown-hour").textContent = String(hours).padStart(2, "0");
+    document.querySelector(".countdown-minute").textContent = String(minutes).padStart(2, "0");
+    document.querySelector(".countdown-second").textContent = String(seconds).padStart(2, "0");
 
     // Check if the countdown has finished
     if (time <= 0) {
       clearInterval(countdownInterval);
       //play a sound. Beep beep beep
-      console.log("Time is up!");
+      console.log('Time is up!')
     }
   }, 1000);
 }
