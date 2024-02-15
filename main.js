e39a3332f0a9f4445b074846860840ed33008831
Stephen <stephenchow1111@gmail.com>
2024-02-15 20:05:04 +0800

    count down timer almost finished

--- main.js
+++ main.js
@@ -5,10 +5,45 @@
 // get the current time
 // subtract the countdown time --> question: is it gonna be in min? sec? milisec?
 // display the countdown on the countdown box
-const currTime = new Date();
-console.log(currTime);
 // convert hours and minutes into seconds
 
 // convert the user input to seconds
 // conditional: check if current time = current time + input time
 // if so, execute the code
+
+const secondInput = document.querySelector("#second-input");
+const minuteInput = document.querySelector("#minute-input");
+const hourInput = document.querySelector("#hour-input");
+const startButton = document.querySelector("#btn");
+
+startButton.addEventListener("click", function () {
+  const seconds = parseInt(secondInput.value) || 0;
+  const minutes = parseInt((minuteInput.value) || 0) * 60;
+  const hours = parseInt((hourInput.value) || 0) * 60 * 60;
+  const totalSeconds = Number(seconds + minutes + hours);
+ 
+  startCountdown(totalSeconds);
+});
+
+function startCountdown(time) {
+  const countdownInterval = setInterval(() => {
+    time -= 1;
+
+    const hours = Math.floor(time / 3600);
+    const minutes = Math.floor((time % 3600) / 60);
+    const seconds = time % 60;
+
+    console.log('Seconds: ', seconds);
+    console.log('minutes: ', minutes);
+    
+    document.querySelector(".countdown-hour").textContent = String(hours).padStart(2, "0");
+    document.querySelector(".countdown-minute").textContent = String(minutes).padStart(2, "0");
+    document.querySelector(".countdown-second").textContent = String(seconds).padStart(2, "0");
+
+    // Check if the countdown has finished
+    if (time <= 0) {
+      clearInterval(countdownInterval); // Stop the countdown
+      console.log("Countdown finished!");
+    }
+  }, 1000); 
+}
